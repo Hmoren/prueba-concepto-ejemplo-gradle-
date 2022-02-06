fa976c2276d66b7134b10b61fe4a6d76e5b8ec4c
Hernan Moreno <hernan.alexis.moreno@gmail.com>
2022-02-06 10:15:35 -0300

    carga inicial

---
+++ src/test/java/com/devopsusach2020/DevOpsUsach2020ApplicationTests.java
@@ -0,0 +1,25 @@
+package com.devopsusach2020;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import com.devopsusach2020.model.Pais;
+
+@SpringBootTest
+class DevOpsUsach2020ApplicationTests {
+
+	@Test
+	void contextLoads() {
+    Pais p = new Pais();
+    assertNotNull(p);
+	}
+
+	@Test
+	void contextLoads2() {
+    Pais p = new Pais();
+    assertNotNull(p);
+	}
+
+}
